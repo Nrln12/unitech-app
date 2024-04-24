982f9c9b487230eeaf38f39c664fea6d30dc1dfe
nurlana12 <nurlanammmdova2@gmail.com>
2024-04-24 17:54:31 +0400

    added other tests

--- src/test/java/com/unitech/app/unitechapp/service/impl/AccountServiceImplTest.java
+++ src/test/java/com/unitech/app/unitechapp/service/impl/AccountServiceImplTest.java
@@ -2,6 +2,8 @@ package com.unitech.app.unitechapp.service.impl;
 
 import com.unitech.app.unitechapp.entity.Account;
 import com.unitech.app.unitechapp.entity.User;
+import com.unitech.app.unitechapp.exception.BadRequestException;
+import com.unitech.app.unitechapp.exception.NotFoundException;
 import com.unitech.app.unitechapp.model.request.AccountRequest;
 import com.unitech.app.unitechapp.model.request.MoneyTransferRequest;
 import com.unitech.app.unitechapp.model.response.AccountResponse;
@@ -101,7 +103,7 @@ public class AccountServiceImplTest {
     }
 
     @Test
-    public void testCreateAccount() {
+    void testCreateAccount() {
         when(userRepository.findByPin("1234")).thenReturn(Optional.of(user));
         when(accountRepository.findAccountByAccountNo("A12345")).thenReturn(Optional.empty());
         when(modelMapper.map(accountRequest, Account.class)).thenReturn(account);
@@ -110,7 +112,7 @@ public class AccountServiceImplTest {
     }
 
     @Test
-    public void testGetAccountByPin() {
+    void testGetAccountByPin() {
         String token = "Bearer 1234";
         String pin = user.getPin();
         Long userId = user.getId();
@@ -136,7 +138,36 @@ public class AccountServiceImplTest {
     }
 
     @Test
-    public void testMoneyTransfer() {
+    void testGetAccountByPin_NotFoundAccount() {
+        String token = "Bearer 1234";
+        String pin = user.getPin();
+        Long userId = user.getId();
+
+        when(jwtService.extractUserName("1234")).thenReturn(pin);
+        when(userRepository.findByPin(pin)).thenReturn(Optional.of(user));
+
+        when(accountRepository.findAccountByUserIdAndStatusIsTrue(userId)).thenReturn(List.of());
+
+        assertThrows(NotFoundException.class, () -> accountService.getAccountsByPin(token));
+        verify(accountRepository, times(1)).findAccountByUserIdAndStatusIsTrue(userId);
+
+    }
+
+    @Test
+    void testGetAccountByPin_NotFoundUser() {
+        String token = "Bearer 1234";
+        String pin = user.getPin();
+
+        when(jwtService.extractUserName("1234")).thenReturn(pin);
+        when(userRepository.findByPin(pin)).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> accountService.getAccountsByPin(token));
+        verify(userRepository, times(1)).findByPin(pin);
+
+    }
+
+    @Test
+    void testMoneyTransfer() {
         Double fromAccBalance = fromAcc.getBalance();
         Double toAccBalance = toAcc.getBalance();
         Double amount = moneyTransferRequest.getAmount();
@@ -162,5 +193,82 @@ public class AccountServiceImplTest {
         verify(accountRepository).findAccountByAccountNoAndStatusIsTrue(fromAcc.getAccountNo());
         verify(accountRepository).findAccountByAccountNoAndStatusIsTrue(toAcc.getAccountNo());
     }
+
+    @Test
+    void testMoneyTransfer_NotActiveFromAccountOrNull() {
+        when(accountRepository.findAccountByAccountNoAndStatusIsTrue(fromAcc.getAccountNo())).thenReturn(Optional.empty());
+        assertThrows(NotFoundException.class, () -> accountService.moneyTransfer(moneyTransferRequest));
+        verify(accountRepository, times(1)).findAccountByAccountNoAndStatusIsTrue(fromAcc.getAccountNo());
+
+    }
+
+    @Test
+    void testMoneyTransfer_NotActiveToAccountOrNull() {
+
+        when(accountRepository.findAccountByAccountNoAndStatusIsTrue(fromAcc.getAccountNo())).thenReturn(Optional.of(fromAcc));
+
+        when(accountRepository.findAccountByAccountNoAndStatusIsTrue(toAcc.getAccountNo())).thenReturn(Optional.empty());
+        assertThrows(NotFoundException.class, () -> accountService.moneyTransfer(moneyTransferRequest));
+        verify(accountRepository, times(1)).findAccountByAccountNoAndStatusIsTrue(fromAcc.getAccountNo());
+        verify(accountRepository, times(1)).findAccountByAccountNoAndStatusIsTrue(toAcc.getAccountNo());
+
+    }
+
+    @Test
+    void testMoneyTransfer_SameAccount() {
+        moneyTransferRequest = MoneyTransferRequest
+                .builder()
+                .fromAccountNo(fromAcc.getAccountNo()) // same acc
+                .toAccountNo(fromAcc.getAccountNo()) // same acc
+                .amount(5.0)
+                .build();
+        when(accountRepository.findAccountByAccountNoAndStatusIsTrue(fromAcc.getAccountNo())).thenReturn(Optional.of(fromAcc));
+        when(accountRepository.findAccountByAccountNoAndStatusIsTrue(fromAcc.getAccountNo())).thenReturn(Optional.of(fromAcc));
+
+        assertThrows(BadRequestException.class, () -> accountService.moneyTransfer(moneyTransferRequest));
+        verify(accountRepository, times(2)).findAccountByAccountNoAndStatusIsTrue(fromAcc.getAccountNo());
+    }
+
+    @Test
+    void testMoneyTransfer_InvalidAmount() {
+
+        moneyTransferRequest = MoneyTransferRequest
+                .builder()
+                .fromAccountNo(fromAcc.getAccountNo())
+                .toAccountNo(toAcc.getAccountNo())
+                .amount(-1.0) //invalid amount
+                .build();
+
+        when(accountRepository.findAccountByAccountNoAndStatusIsTrue(fromAcc.getAccountNo())).thenReturn(Optional.of(fromAcc));
+        when(accountRepository.findAccountByAccountNoAndStatusIsTrue(toAcc.getAccountNo())).thenReturn(Optional.of(toAcc));
+
+
+        assertThrows(BadRequestException.class, () -> accountService.moneyTransfer(moneyTransferRequest));
+
+        verify(accountRepository, times(1)).findAccountByAccountNoAndStatusIsTrue(fromAcc.getAccountNo());
+        verify(accountRepository, times(1)).findAccountByAccountNoAndStatusIsTrue(toAcc.getAccountNo());
+    }
+
+    @Test
+    void testMoneyTransfer_NotEnoughBalance() {
+
+        moneyTransferRequest = MoneyTransferRequest
+                .builder()
+                .fromAccountNo(fromAcc.getAccountNo())
+                .toAccountNo(toAcc.getAccountNo())
+                .amount(130.0) //invalid amount according to current balance
+                .build();
+
+        when(accountRepository.findAccountByAccountNoAndStatusIsTrue(fromAcc.getAccountNo())).thenReturn(Optional.of(fromAcc));
+        when(accountRepository.findAccountByAccountNoAndStatusIsTrue(toAcc.getAccountNo())).thenReturn(Optional.of(toAcc));
+
+
+        assertThrows(BadRequestException.class, () -> accountService.moneyTransfer(moneyTransferRequest));
+
+        verify(accountRepository, times(1)).findAccountByAccountNoAndStatusIsTrue(fromAcc.getAccountNo());
+        verify(accountRepository, times(1)).findAccountByAccountNoAndStatusIsTrue(toAcc.getAccountNo());
+    }
+
+
 }
 
--- src/test/java/com/unitech/app/unitechapp/service/impl/AuthServiceImplTest.java
+++ src/test/java/com/unitech/app/unitechapp/service/impl/AuthServiceImplTest.java
@@ -2,6 +2,9 @@ package com.unitech.app.unitechapp.service.impl;
 
 import com.unitech.app.unitechapp.entity.User;
 import com.unitech.app.unitechapp.exception.AlreadyExistsException;
+import com.unitech.app.unitechapp.exception.BadRequestException;
+import com.unitech.app.unitechapp.exception.NotFoundException;
+import com.unitech.app.unitechapp.model.request.LoginRequest;
 import com.unitech.app.unitechapp.model.request.RegisterRequest;
 import com.unitech.app.unitechapp.model.response.AuthResponse;
 import com.unitech.app.unitechapp.repository.UserRepository;
@@ -12,6 +15,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
@@ -32,10 +37,12 @@ public class AuthServiceImplTest {
     private JwtService jwtService;
 
     @Mock
-    private ModelMapper modelMapper; // Mock ModelMapper dependency
+    private ModelMapper modelMapper;
 
     @InjectMocks
     private AuthServiceImpl authService;
+    @Mock
+    private AuthenticationManager authenticationManager;
 
     @BeforeEach
     public void setUp() {
@@ -86,4 +93,54 @@ public class AuthServiceImplTest {
         verify(userRepository, never()).save(any());
     }
 
+    @Test
+    public void testLogin() {
+        LoginRequest request = new LoginRequest();
+        request.setPin("123456");
+        request.setPassword("Password123!");
+        User user = User.builder()
+                .pin("123456")
+                .password("Password123!")
+                .build();
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getPin(), request.getPassword()));
+        when(userRepository.findByPin(request.getPin())).thenReturn(Optional.of(user));
+
+        when(passwordEncoder.encode(request.getPassword())).thenReturn("encodedPassword");
+
+        when(jwtService.generateToken(any(User.class))).thenReturn("mockedToken");
+
+        AuthResponse response = authService.login(request);
+
+
+        verify(userRepository, times(1)).findByPin(request.getPin());
+
+        assertNotNull(response.getToken());
+    }
+
+    @Test
+    public void testLogin_NullPointer() {
+        LoginRequest request = new LoginRequest();
+        request.setPin(null);
+        request.setPassword("Password123!");
+        assertThrows(BadRequestException.class, () -> authService.login(request));
+    }
+
+    @Test
+    public void testLogin_NotFound() {
+        LoginRequest request = new LoginRequest();
+        request.setPin("123456");
+        request.setPassword("Password123!");
+
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getPin(), request.getPassword()));
+        when(userRepository.findByPin(request.getPin())).thenReturn(Optional.empty());
+
+        when(passwordEncoder.encode(request.getPassword())).thenReturn("encodedPassword");
+
+        when(jwtService.generateToken(any(User.class))).thenReturn("mockedToken");
+
+        assertThrows(NotFoundException.class, () -> authService.login(request));
+
+        verify(userRepository, times(1)).findByPin(request.getPin());
+
+    }
 }

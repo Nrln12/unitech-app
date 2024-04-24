f2e933436369383293ef95baa49beba42da67aa4
nurlana12 <nurlanammmdova2@gmail.com>
2024-04-24 15:25:12 +0400

    refactor

--- src/main/java/com/unitech/app/unitechapp/service/impl/CurrencyServiceImpl.java
+++ src/main/java/com/unitech/app/unitechapp/service/impl/CurrencyServiceImpl.java
@@ -4,33 +4,32 @@ import com.unitech.app.unitechapp.model.response.CurrencyRateResponse;
 import com.unitech.app.unitechapp.service.CurrencyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
 public class CurrencyServiceImpl implements CurrencyService {
     private static final String API_BASE_URL = "https://v6.exchangerate-api.com/v6/";
-    @Value("${api.key}")
-    private String apiKey;
+    private String apiKey  = "db41ec93e26061809ae3b62f";
     private final RestTemplate restTemplate;
 
     @Override
     public CurrencyRateResponse getCurrencyRate(String source, String target, Double amount) {
         String url = API_BASE_URL + apiKey + "/pair/" + source + "/" + target + "/" + amount;
 
-        ResponseEntity<CurrencyRateResponse> responseEntity = new RestTemplate().getForEntity(url, CurrencyRateResponse.class);
-        if (responseEntity.getStatusCode() == HttpStatus.OK) {
-            CurrencyRateResponse response = responseEntity.getBody();
+        try {
+            CurrencyRateResponse response = restTemplate.getForObject(url, CurrencyRateResponse.class);
+            System.out.println(url);
             if (response != null && response.getResult().equals("success")) {
                 return response;
-            } else {
-                throw new RuntimeException("Failed to fetch currency rate: " + responseEntity.getBody());
             }
-        } else {
-            throw new RuntimeException("Failed to fetch currency rate: HTTP " + responseEntity.getStatusCode());
+            throw new RuntimeException("Failed to fetch currency rate");
+        } catch (HttpClientErrorException.NotFound e) {
+            throw new RuntimeException("Currency pair not found or API endpoint not reachable");
         }
     }
+
 }
+

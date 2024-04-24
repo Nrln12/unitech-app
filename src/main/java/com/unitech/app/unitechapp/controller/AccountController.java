393e82a759bd783eb99dd0950727329b41b4731f
nurlana12 <nurlanammmdova2@gmail.com>
2024-04-24 15:22:20 +0400

    changed response status

--- src/main/java/com/unitech/app/unitechapp/controller/AccountController.java
+++ src/main/java/com/unitech/app/unitechapp/controller/AccountController.java
@@ -3,14 +3,11 @@ package com.unitech.app.unitechapp.controller;
 import com.unitech.app.unitechapp.model.request.AccountRequest;
 import com.unitech.app.unitechapp.model.request.MoneyTransferRequest;
 import com.unitech.app.unitechapp.service.AccountService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpResponse;
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("/api/accounts")
@@ -19,18 +16,19 @@ public class AccountController {
     private final AccountService accountService;
 
     @PostMapping()
-    public ResponseEntity<?> createAccount(@RequestBody AccountRequest request) {
+    @ResponseStatus(CREATED)
+    public void createAccount(@RequestBody AccountRequest request) {
         accountService.createAccount(request);
-        return ResponseEntity.ok(HttpStatus.CREATED);
     }
+
     @GetMapping()
-    public ResponseEntity<?> getAccounts(@RequestHeader(value = "Authorization") String token){
+    public ResponseEntity<?> getAccounts(@RequestHeader(value = "Authorization") String token) {
         return ResponseEntity.ok(accountService.getAccountsByPin(token));
     }
 
     @PostMapping("/transfer")
-    public ResponseEntity<?> moneyTransfer(@RequestBody MoneyTransferRequest request) {
+    @ResponseStatus(CREATED)
+    public void moneyTransfer(@RequestBody MoneyTransferRequest request) {
         accountService.moneyTransfer(request);
-        return ResponseEntity.ok(HttpStatus.CREATED);
     }
 }
